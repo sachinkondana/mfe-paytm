eda12f1cfa92baece120d0e748a8a39ddce32b54
skondana <skondana@enphaseenergy.com>
2021-09-11 11:59:50 +0530

    Added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 export default () => {
   return (
+    <BrowserRouter>
     <div>
-      <h1>Hi there!!!</h1>
-      <h1>Feature 1</h1>
-      <hr />
+      <Header />
       <MarketingApp />
     </div>
+    </BrowserRouter>
   );
 };
