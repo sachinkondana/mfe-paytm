0ec2ae7195125c64b5a975fe22177a0809b03437
skondana <skondana@enphaseenergy.com>
2021-09-12 09:41:31 +0530

    fix(auth): add auth modules

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -5,7 +5,7 @@ import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el, { location='', onNavigate, defaultHistory }) => {
+const mount = (el, { onSignIn, location='', onNavigate, defaultHistory }) => {
   const history = defaultHistory || createMemoryHistory({
     initialEntries: [location],
   });
@@ -14,7 +14,7 @@ const mount = (el, { location='', onNavigate, defaultHistory }) => {
     history.listen(onNavigate);
   }
 
-  ReactDOM.render(<App history={history} />, el);
+  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
@@ -31,7 +31,7 @@ const mount = (el, { location='', onNavigate, defaultHistory }) => {
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
     mount(devRoot,  {
